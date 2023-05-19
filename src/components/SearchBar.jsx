1e3a0f17640d34b00ba4c2b4a7fa6852b297243d
Lucas Wade <lucaswade@Lucass-MacBook-Air.local>
2023-05-19 16:18:38 -0700

    Implement show more to show 5 movies instead of one, improve styling slightly

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,6 +8,7 @@ const SearchBar = () => {
     const [searchInput, setSearchInput] = useState("")
     const [searchResults, setSearchResults] = useState([])
     const [displayExtended, setDisplayExtended] = useState(false)
+    const [hide, setHide] = useState(false)
 
     const handleMovieSearchSubmit = function(e) {
         e.preventDefault();
@@ -30,8 +31,10 @@ const SearchBar = () => {
     const handleShowMore = function() {
       if (displayExtended !== true) {
         setDisplayExtended(true)
+        setHide(true)
       } else {
         setDisplayExtended(false)
+        setHide(false)
       }
     }
 
@@ -45,13 +48,13 @@ const SearchBar = () => {
       }, [searchInput]);
 
   return (
-    <div className='flex w-screen justify-center items-center'>
+    <div className='flex w-screen justify-center items-center bg-gray-100 pb-5'>
       <form id="search" className='flex flex-col justify-center items-center' onSubmit={handleMovieSearchSubmit}>
         <label htmlFor="search-input" className='text-purple-800'>Search</label>
-        <input type='text' id="search-input" placeholder='search for a movie...' className='w-96 h-24 rounded-2xl text-black' value={searchInput} onChange={handleMovieSearchChange} />
-        {searchResults && searchResults.length > 1 && (
+        <input type='text' id="search-input" placeholder='search for a movie...' className='w-96 h-24 rounded-2xl text-black mb-6' value={searchInput} onChange={handleMovieSearchChange} />
+        {!hide && searchResults && searchResults.length > 1 && (
             <div className='text-black w-72 h-36 bg-zinc-400 rounded-lg border-zinc-900 border-solid border p-1 flex'>
-                {searchResults["Poster"] !== "N/A" &&(
+                {searchResults["Poster"] !== "N/A" && (
                     <img alt={searchResults[0]["Title"]} src={searchResults[0]["Poster"]} height="75px" width="90px"/>
                 )}
                 <div className="w-full flex flex-col items-center justify-center px-1 text-center">
@@ -66,8 +69,13 @@ const SearchBar = () => {
                 searchResults.map((ele, i) => {
                   if (i <= 5) {
                     return (
-                      <div key={i} className='text-black'>
-                        <h1>{ele["Title"]}</h1>
+                      <div key={i} className='text-black w-full h-36 bg-zinc-400 rounded-lg border-zinc-900 border-solid border p-1 flex -mx-6 mb-2'>
+                        {ele["Poster"] !== "N/A" ? <img alt={ele["Title"]} src={ele["Poster"]} height="75px" width="90px"/> : <div className='h-[75px] w-[90px]'>No Image Available</div>}
+                          <div className="w-full flex flex-col items-center justify-center px-1 text-center">
+                            <h1>{ele["Title"]}</h1>
+                            <h2>{ele.Year}</h2>
+                            <a target="_blank" href={`https://www.imdb.com/title/${ele.imdbID}/`}>IMDB</a>
+                          </div>
                       </div>
                     )
                   }
