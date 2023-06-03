03a8d90bf69569897e6c7f5efef1d3837f99fde2
Lucas Wade <lucaswade@Lucass-MacBook-Air.local>
2023-06-02 17:24:32 -0700

    Add form for index page, styling

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import NavBar from '@/components/NavBar'
 import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 
 const index = () => {
   return (
     <div className='h-screen bg-zinc-700'>
         <NavBar />
-        <div className='h-1/4 w-full bg-zinc-400 flex text-zinc-900 justify-center py-4 px-1'>
+        <div className='h-1/4 w-full bg-zinc-400 flex flex-col text-zinc-900 items-center justify-between py-4 px-1'>
           <h1 className='text-2xl text-center'>Get AI generated movie recommendations!</h1>
-          <Button variant="contained">Test</Button>
+        </div>
+        <div className='m-4 flex justify-center'>
+          <form className=' w-2/3'>
+            <TextField id="vibe-input" sx={{ input: { color: 'white' } }} fullWidth InputLabelProps={{className: "text-white"}} FormHelperTextProps={{className: "text-white"}} label="Vibe" variant='filled' color="secondary" helperText="Enter the vibe of the movie you want" className='text-white'/>
+            <Button variant="contained" color='secondary' size='small' className='w-[50px] h-[50px]'>Test</Button>
+          </form>
         </div>
     </div>
   )
